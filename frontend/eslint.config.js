2d37eaedf36139bb99e443f15ec2af68bb1338f3
Pham Doanh <pgdoanh711@gmail.com>
2026-04-16 00:50:14 +0700

    fix: script linting code does not working

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginReactRecommended from "eslint-plugin-react/configs/recommended.js";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, react: pluginReact },
+    extends: ["js/recommended", pluginReactRecommended],
+    languageOptions: { globals: globals.browser },
+    settings: { react: { version: "detect" } },
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"]
+  },
 ]);
